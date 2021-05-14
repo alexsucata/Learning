4f636b990146be336a5aa02139759b2e2d46665a
alexsucata <alex.sucata@yahoo.com>
2021-05-14 13:47:18 +0300

    Initial commit

---
+++ src/com/alexsucata/arrays/Main.java
@@ -0,0 +1,9 @@
+package com.alexsucata.arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/com/alexsucata/arrays/MobilePhone.java
@@ -0,0 +1,31 @@
+package com.alexsucata.arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MobilePhone {
+    private String myNumber;
+    private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<Contact>();
+    }
+
+    public boolean addNewContact(Contact contact) {
+        if (findContact(contact.getName()) >= 0) {
+            System.out.println("Contact is already on file");
+            return false;
+        }
+        myContacts.add(contact);
+        return true;
+    }
+
+    private int findContact(Contact contact) {
+        return this.myContacts.indexOf(contact);
+    }
+
+    private int findContact(String contactName) {
+
+    }
+}
