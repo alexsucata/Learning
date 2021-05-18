8cd8ebc4844d25c921fd700a05880d4e5adbdfcd
alexsucata <alex.sucata@yahoo.com>
2021-05-18 23:57:47 +0300

    Main class completed. App tested

--- src/com/alexsucata/arrays/MobilePhone.java
+++ src/com/alexsucata/arrays/MobilePhone.java
@@ -32,7 +32,7 @@ public class MobilePhone {
         return true;
     }
 
-    public boolean removeContact (Contact contact){
+    public boolean removeContact(Contact contact) {
         int foundPosition = findContact(contact);
         if (foundPosition < 0) {
             System.out.println(contact.getName() + " was not found.");
@@ -57,10 +57,27 @@ public class MobilePhone {
         return -1;
     }
 
-    public String queryContact (Contact contact){
-        if (findContact(contact)>=0){
+    public String queryContact(Contact contact) {
+        if (findContact(contact) >= 0) {
             return contact.getName();
         }
         return null;
+    }
+
+    public Contact queryContact (String name){
+        int position = findContact(name);
+        if(position>=0){
+            return this.myContacts.get(position);
+        }
+        return null;
+    }
+
+    public void printContacts() {
+        System.out.println("Contact List");
+        for (int i = 0; i < this.myContacts.size(); i++) {
+            System.out.println((i + 1) + "." +
+                    this.myContacts.get(i).getName() + " -> " +
+                    this.myContacts.get(i).getPhoneNumber());
+        }
     }
 }
